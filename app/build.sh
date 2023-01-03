95948ec790fc248bd047dc919818d75501b894ca
unknown <savinov.vladislav@gmail.com>
2023-01-03 17:59:52 +0300

    clang migration

--- app/build.sh
+++ app/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
-conan install . -if conan_install --build=missing -s build_type=$1
+
+conan install . --profile clang.conan -if conan_install -b missing -s build_type=$1
 cmake -S . -B build -DCMAKE_BUILD_TYPE=$1
 cmake --build build
 cd build
